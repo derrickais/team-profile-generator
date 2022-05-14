7fb39f52ceea7ed96513930707342671d9af7762
Derricka S <derrickas728@gmail.com>
2022-05-14 17:41:57 -0400

    add Employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require("../lib/Employee")
+
+test("creates an employee object", () => {
+    const employee = new Employee("John", "1", "email@email.com");
+
+    expect(employee.name).toBe("John");
+    expect(employee.id).toBe("1");
+    expect(employee.email).toBe("email@email.com");
+});
